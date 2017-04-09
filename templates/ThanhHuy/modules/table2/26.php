b4af156fd427bc78d5e8312340eff69711325385
thanhhuypham <thanhhuy.25101996@gmail.com>
2017-04-09 11:57:04 +0700

    Update module

--- templates/ThanhHuy/modules/table2/26.php
+++ templates/ThanhHuy/modules/table2/26.php
@@ -11,7 +11,6 @@
 
         <link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="../../css/font-awesome.min.css">
-        <link href="../../css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css"/>
 
         <?php
         if (!class_exists('lessc')) {
@@ -23,10 +22,10 @@
         <link rel="stylesheet" href="../../css/table2/26.css">
         <script src="../../js/table2/26/jquery-2.1.4.min.js" type="text/javascript"></script>
         <script src="../../js/table2/26/bootstrap.min.js" type="text/javascript"></script>
-
-        <script src="../../js/table2/26/jquery.dataTables.min.js" type="text/javascript"></script>
-        <script src="../../js/table2/26/dataTables.bootstrap.min.js" type="text/javascript"></script>
-        <script src="../../js/table2/26/custom-script.js" type="text/javascript"></script>
+        <script src="../../js/table2/26/pagination-table.js" type="text/javascript"></script>
+        <script src="../../js/table2/26/search-table.js" type="text/javascript"></script>
+        <script src="../../js/table2/26/close-panel.js" type="text/javascript"></script>
+        <script src="../../js/table2/26/collapse-panel.js" type="text/javascript"></script>
 
     </head>
 
@@ -43,12 +42,12 @@
                                 </h3>
                                 <ul class="items">
                                     <li>
-                                        <a class="collapse-link">
+                                        <a class="toggled" id="collapse-link">
                                             <i class="fa fa-chevron-up"></i>
                                         </a>
                                     </li>
                                     <li class="dropdown">
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                        <a href="#" data-toggle="dropdown">
                                             <i class="fa fa-wrench"></i>
                                         </a>
                                         <ul class="dropdown-menu">
@@ -57,7 +56,7 @@
                                         </ul>
                                     </li>
                                     <li>
-                                        <a class="close-link">
+                                        <a class="toggled" id="close-link">
                                             <i class="fa fa-close"></i>
                                         </a>
                                     </li>
@@ -68,19 +67,56 @@
                                 <p>KeyTable provides Excel like cell navigation on any table. Events (focus, blur, action etc) can be assigned to individual cells, columns, rows or all cells.</p>
                                 <div class="row">
                                     <div class="col-md-12 col-sm-12 col-xs-12">
-                                        <table id="datatable-keytable" class="table table-bordered">
+                                        <div class="show-data">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <span class="data">
+                                                        Show 
+                                                        <select class="form-control form-select">
+                                                            <option value="5">5</option>
+                                                            <option value="5">10</option>
+                                                            <option value="5">15</option>
+                                                            <option value="5">20</option>
+                                                        </select>
+                                                        entries 
+                                                    </span>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <span class="search-query">
+                                                        Search: <input type="search" id="input" onkeyup="searchDataTable();" class="form-control form-search" placeholder="Search for name..." title="Type in a name">
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <table class="data-table table table-bordered" id="table">
                                             <thead>
-                                                <tr role="row">
-                                                    <th tabindex="0" style="width: 241px;">Name</th>
-                                                    <th tabindex="0" style="width: 261px;">Position</th>
-                                                    <th tabindex="0" style="width: 118px;">Office</th>
-                                                    <th tabindex="0" style="width: 59px;">Age</th>
-                                                    <th tabindex="0" style="width: 113px;">Start date</th>
-                                                    <th tabindex="0" style="width: 87px;">Salary</th>
+                                                <tr>
+                                                    <th onclick="sortTable(0)">Name</th>
+                                                    <th onclick="sortTable(1)">Position</th>
+                                                    <th onclick="sortTable(2)">Office</th>
+                                                    <th onclick="sortTable(3)">Age</th>
+                                                    <th onclick="sortTable(4)">Start date</th>
+                                                    <th onclick="sortTable(5)">Salary</th>
                                                 </tr>
                                             </thead>
-                                            <tbody>
-                                                <tr role="row" class="odd">
+                                            <tbody id="myDataTable">
+                                                <tr>
+                                                    <td>Ashton Cox</td>
+                                                    <td>Junior Technical Author</td>
+                                                    <td>San Francisco</td>
+                                                    <td>66</td>
+                                                    <td>2009/01/12</td>
+                                                    <td>$86,000</td>
+                                                </tr>
+                                                <tr >
+                                                    <td>Bradley Greer</td>
+                                                    <td>Software Engineer</td>
+                                                    <td>London</td>
+                                                    <td>41</td>
+                                                    <td>2012/10/13</td>
+                                                    <td>$132,000</td>
+                                                </tr>
+                                                <tr>
                                                     <td>Brenden Wagner</td>
                                                     <td>Software Engineer</td>
                                                     <td>San Francisco</td>
@@ -96,7 +132,7 @@
                                                     <td>2012/12/02</td>
                                                     <td>$372,000</td>
                                                 </tr>
-                                                <tr role="row" class="odd">
+                                                <tr>
                                                     <td>Bruno Nash</td>
                                                     <td>Software Engineer</td>
                                                     <td>London</td>
@@ -112,22 +148,6 @@
                                                     <td>2011/12/12</td>
                                                     <td>$106,450</td>
                                                 </tr>
-                                                <tr role="row" class="odd">
-                                                    <td>Cara Stevens</td>
-                                                    <td>Sales Assistant</td>
-                                                    <td>New York</td>
-                                                    <td>46</td>
-                                                    <td>2011/12/06</td>
-                                                    <td>$145,600</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Cedric Kelly</td>
-                                                    <td>Senior Javascript Developer</td>
-                                                    <td>Edinburgh</td>
-                                                    <td>22</td>
-                                                    <td>2012/03/29</td>
-                                                    <td>$433,060</td>
-                                                </tr>
                                                 <tr>
                                                     <td>Cara Stevens</td>
                                                     <td>Sales Assistant</td>
@@ -144,8 +164,49 @@
                                                     <td>2012/03/29</td>
                                                     <td>$433,060</td>
                                                 </tr>
+                                                <tr>
+                                                    <td>Cara Stevens</td>
+                                                    <td>Sales Assistant</td>
+                                                    <td>New York</td>
+                                                    <td>46</td>
+                                                    <td>2011/12/06</td>
+                                                    <td>$145,600</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Cedric Kelly</td>
+                                                    <td>Senior Javascript Developer</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>22</td>
+                                                    <td>2012/03/29</td>
+                                                    <td>$433,060</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Cedric Kelly</td>
+                                                    <td>Senior Javascript Developer</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>22</td>
+                                                    <td>2012/03/29</td>
+                                                    <td>$433,060</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Cedric Kelly</td>
+                                                    <td>Senior Javascript Developer</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>22</td>
+                                                    <td>2012/03/29</td>
+                                                    <td>$433,060</td>
+                                                </tr>
                                             </tbody>
                                         </table>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <p>Showing 1 to 5 of 12 entries</p>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <ul class="pagination page" id="myPager">
+                                                </ul>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
