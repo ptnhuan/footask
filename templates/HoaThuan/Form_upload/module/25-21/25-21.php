e45d798d8bd8d26604c7b22e1bbc51e4b2aeedea
Thuan <nguyenthuan9613@gmail.com>
2017-04-10 12:54:20 +0700

    admin template

---
+++ templates/HoaThuan/Form_upload/module/25-21/25-21.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/25-21.less', 'css/25-21.css');
+        ?>
+        <link href="css/25-21.css" rel="stylesheet" type="text/css" />
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/demo.js" type="text/javascript"></script>
+        <script src="js/nump1.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="type-25-21">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-2">
+                    </div>
+                    <div class="col-md-10">
+                        <div class="row content2">
+                            <div class="col-md-12 col-sm-12 col-xs-12">
+                                <div class="x_panel">
+                                    <div class="x_title">
+                                        <h2>Dropzone multiple file uploader</h2>
+                                        <ul class="nav navbar-right panel_toolbox">
+                                            <li><a data-toggle="collapse" href="#upload" ><i class="fa fa-chevron-up"></i></a>
+                                            </li>
+                                            <li class="dropdown">
+                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                                <ul class="dropdown-menu" role="menu">
+                                                    <li><a href="#">Settings 1</a>
+                                                    </li>
+                                                    <li><a href="#">Settings 2</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                            </li>
+                                        </ul>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div id="upload">
+                                        <p>Drag multiple files to the box below for multi upload or click to select files. 
+                                            This is for demonstration purposes only, the files are not uploaded to any server.</p>
+                                        <form class="dropzone dz-clickable">
+                                            <div class="dz-message">
+                                                <span>Drop files here to upload</span>
+                                            </div>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </body>
+
+</html>
