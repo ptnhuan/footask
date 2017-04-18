8f92a83f20a30c0d6f15a332b5f28c34ddeff8fa
thanhhuypham <thanhhuy.25101996@gmail.com>
2017-04-18 11:21:17 +0700

    Commit new admin template module invoce

---
+++ templates/ThanhHuy/modules/invoice/58.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Module - Sidebar left profile</title>
+
+        <link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="../../css/font-awesome.min.css">
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('../../libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('../../less/invoice/58.less', '../../css/invoice/58.css');
+        ?>
+        <link rel="stylesheet" href="../../css/invoice/58.css">
+        <script src="../../js/invoice/58/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="../../js/invoice/58/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+        <div class="type-58">
+            <div class="container-fluid">
+                <div class="nav-side-menu col-md-2">
+                    <div class="logo">
+                        <a href="#">
+                            <i class="fa fa-paw"></i>
+                            <span>Gentelella Alela!</span>
+                        </a>
+                    </div>
+                    <div class="img-user clearfix">
+                        <img src="../../images/img.jpg" alt="men">
+                        <div class="welcome-name">
+                            <span>Welcome,</span>
+                            <p>John Due</p>
+                        </div>
+                    </div>
+                    <div class="sidebar-menu">
+                        <h3>General</h3>
+                        <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+
+                        <div class="menu-list">
+                            <ul id="menu-content" class="menu-content collapse out">
+                                <li data-toggle="collapse" data-target="#home" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-home"></i> Home <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="home">
+                                    <li><a href="#">&#8226; Dashboard</a></li>
+                                    <li><a href="#">&#8226; Dashboard2</a></li>
+                                    <li><a href="#">&#8226; Dashboard3</a></li>
+                                </ul>
+
+                                <li data-toggle="collapse" data-target="#form" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-edit"></i> Forms <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="form">
+                                    <li><a href="#">&#8226; General Form</a></li>
+                                    <li><a href="#">&#8226; Advanced Components</a></li>
+                                    <li><a href="#">&#8226; Form Validation</a></li>
+                                    <li><a href="#">&#8226; Form Wizard</a></li>
+                                    <li><a href="#">&#8226; Form Upload</a></li>
+                                    <li><a href="#">&#8226; Form Buttons</a></li>
+                                </ul>
+
+                                <li data-toggle="collapse" data-target="#uie" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-desktop"></i>  UI Elements  <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="uie">
+                                    <li><a href="#">&#8226; General Elements</a></li>
+                                    <li><a href="#">&#8226; Media Gallery</a></li>
+                                    <li><a href="#">&#8226; Typography</a></li>
+                                    <li><a href="#">&#8226; Icons</a></li>
+                                    <li><a href="#">&#8226; Glyphicons</a></li>
+                                    <li><a href="#">&#8226; Widgets</a></li>
+                                    <li class="active"><a href="#">&#8226; Invoice</a></li>
+                                    <li><a href="#">&#8226; Inbox</a></li>
+                                    <li><a href="#">&#8226; Calendar</a></li>
+                                </ul>
+
+                                <li data-toggle="collapse" data-target="#tables" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-table"></i> Tables <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="tables">
+                                    <li><a href="#">&#8226; Tables</a></li>
+                                    <li><a href="#">&#8226; Table Dynamic</a></li>
+                                </ul>
+
+                                <li data-toggle="collapse" data-target="#chart" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-bar-chart-o"></i> Data Presentation  <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="chart">
+                                    <li><a href="#">&#8226; Chart JS</a></li>
+                                    <li><a href="#">&#8226; Chart JS2</a></li>
+                                    <li><a href="#">&#8226; Moris JS</a></li>
+                                    <li><a href="#">&#8226; Echarts</a></li>
+                                    <li><a href="#">&#8226; Other Charts</a></li>
+                                </ul>
+
+                                <li data-toggle="collapse" data-target="#layout" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-clone"></i>  Layouts  <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="layout">
+                                    <li><a href="#">&#8226; Fixed Sidebar</a></li>
+                                    <li><a href="#">&#8226; Fixed Footer</a></li>
+                                </ul>
+                                <h3>Live on</h3>
+
+                                <li data-toggle="collapse" data-target="#pages" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-pagelines"></i>  Additional Pages  <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="pages">
+                                    <li><a href="#">&#8226; E-commerce</a></li>
+                                    <li><a href="#">&#8226; Projects</a></li>
+                                    <li><a href="#">&#8226; Project Detail</a></li>                                    
+                                    <li><a href="#">&#8226; Contacts</a></li>
+                                    <li><a href="#">&#8226; Profile</a></li>
+                                </ul>
+
+                                <li data-toggle="collapse" data-target="#extras" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-windows"></i>  Extras  <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="extras">
+                                    <li><a href="#">&#8226; 403 Error</a></li>
+                                    <li><a href="#">&#8226; 404 Error</a></li>
+                                    <li><a href="#">&#8226; 500 Error</a></li>                                    
+                                    <li><a href="#">&#8226; Plain Page</a></li>
+                                    <li><a href="#">&#8226; Login Page</a></li>
+                                    <li><a href="#">&#8226; Pricing Tables</a></li>
+                                </ul>
+
+                                <li data-toggle="collapse" data-target="#menu" class="collapsed active">
+                                    <a href="#">
+                                        <i class="fa fa-sitemap"></i>   Multilevel Menu   <i class="fa fa-chevron-down arrow"></i>
+                                    </a>
+                                </li>
+                                <ul class="sub-menu collapse" id="menu">
+                                    <li><a href="#">&#8226; Level One</a></li>
+                                    <li><a href="#">&#8226; Level One</a></li>
+                                    <li><a href="#">&#8226; Level One</a></li>    
+                                </ul>
+
+                                <li class="active">
+                                    <a href="#">
+                                        <i class="fa fa-laptop"></i>  Landing Page 
+                                        <span class="label label-success pull-right" style="margin-top: 11px;">Coming Soon</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-footer">
+                            <a href="#" title="Settings">
+                                <i class="glyphicon glyphicon-cog"></i>
+                            </a>
+                            <a href="#" title="Full screen">
+                                <i class="glyphicon glyphicon-fullscreen"></i>
+                            </a>
+
+                            <a href="#" title="Lock">
+                                <i class="glyphicon glyphicon-eye-close"></i>
+                            </a>
+                            <a href="#" title="Logout">
+                                <i class="glyphicon glyphicon-off"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ templates/ThanhHuy/modules/invoice/60.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Module - Content pricing table</title>
+
+        <link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="../../css/font-awesome.min.css">
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('../../libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('../../less/invoice/60.less', '../../css/invoice/60.css');
+        ?>
+        <link rel="stylesheet" href="../../css/invoice/60.css">
+        <script src="../../js/invoice/60/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="../../js/invoice/60/bootstrap.min.js" type="text/javascript"></script>
+        <script src="../../js/invoice/60/collapse-close.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+        <div class="type-60">
+            <div class="container">
+                <div class="content">
+                    <div class="content-left">
+                        <h3>Invoice <small>Some examples to get you started</small></h3>
+                    </div>
+                    <div class="content-right">
+                        <div class="input-search">
+                            <form method="get" action="#">
+                                <input type="text" class="form-search" placeholder="Search for...">
+
+                            </form>
+                            <span class="input-btn-search">
+                                <button class="btn-search" type="button">Go!</button>
+                            </span>
+                        </div>
+                    </div>  
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="admin-panel">
+                            <div class="admin-title">
+                                <h3>
+                                    Invoice Design <small>Sample user invoice design</small>
+                                </h3>
+                                <ul class="items">
+                                    <li>
+                                        <a class="collapse-link">
+                                            <i class="fa fa-chevron-up"></i>
+                                        </a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                            <i class="fa fa-wrench"></i>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#">Settings 1</a></li>
+                                            <li><a href="#">Settings 2</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="close-link">
+                                            <i class="fa fa-close"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="admin-content">
+                                <h1 class="pull-left">
+                                    <i class="fa fa-globe"></i> Invoice <h3 class="pull-right">Date: 18/04/2017</h3>
+                                </h1>
+                                <div class="clearfix"></div>
+                                <div class="row">
+                                    <div class="col-md-4 col-sm-4 col-xs-6">
+                                        From<br><b>Iron Admin, Inc.</b><br>795 Freedom Ave, Suite 600 <br>New York, CA 94107 <br>Phone: 1 (804) 123-9876 <br>Email: ironadmin.com
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-6">
+                                        To<br><b>John Doe </b><br>795 Freedom Ave, Suite 600 <br>New York, CA 94107 <br>Phone: 1 (804) 123-9876 <br>Email: ironadmin.com
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-6">
+                                        <b>Invoice #007612 </b><br><br><b>Oder ID: </b>4F3S8J<br> <b>Payment Due: </b>04/22/2017<br> <b>Account: </b>968-34567
+                                    </div>
+                                </div>
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Qty</th>
+                                            <th>Product</th>
+                                            <th>Serial #</th>
+                                            <th style="width: 59%;">Description</th>
+                                            <th>Subtotal</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Call of Duty</td>
+                                            <td>455-981-221</td>
+                                            <td>455-El snort testosterone trophy driving gloves handsome gerry Richardson helvetica tousled street art master testosterone trophy driving gloves handsome gerry Richardson-221</td>
+                                            <td>$64.50</td>
+                                        </tr>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Need for Speed IV</td>
+                                            <td>247-925-726</td>
+                                            <td>Wes Anderson umami biodiesel</td>
+                                            <td>$50.00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Monsters DVD</td>
+                                            <td>735-845-642</td>
+                                            <td>Terry Richardson helvetica tousled street art master, El snort testosterone trophy driving gloves handsome letterpress erry Richardson helvetica tousled</td>
+                                            <td>$10.70</td>
+                                        </tr>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Grown Ups Blue Ray</td>
+                                            <td>422-568-642</td>
+                                            <td>Tousled lomo letterpress erry Richardson helvetica tousled street art master helvetica tousled street art master, El snort testosterone</td>
+                                            <td>$25.99</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <div class="row">
+                                    <div class="col-md-6 col-sm-6 col-xs-6">
+                                        <h4>Payment Methods:</h4>
+                                        <a href="#"><img src="../../images/visa.png"></a>
+                                        <a href="#"><img src="../../images/mastercard.png"></a>
+                                        <a href="#"><img src="../../images/american-express.png"></a>
+                                        <a href="#"><img src="../../images/paypal.png"></a>
+                                        <div class="box-note">
+                                            <p>Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem plugg dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.</p>
+                                        </div>
+
+                                    </div>
+                                    <div class="col-md-6 col-sm-6 col-xs-6">
+                                        <h4>Amount Due 04/22/2017</h4>
+                                        <table class="table table-striped">
+                                            <tbody>
+                                                <tr>
+                                                    <td>Subtotal:</td>
+                                                    <td>$250.30</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Tax (9.3%)</td>
+                                                    <td>$10.34</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Shipping:</td>
+                                                    <td>$5.80</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Total:</td>
+                                                    <td>$265.24</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+
+                                    </div>                                    
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="pull-left">
+                                            <a href="#" class="btn-print">
+                                                <i class="fa fa-print"> Print</i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="pull-right">
+                                            <a href="#" class="btn-function btn-download-pdf">
+                                                <i class="fa fa-download"> Generate PDF</i>
+                                            </a>
+                                            <a href="#" class="btn-function btn-submit-payment">
+                                                <i class="fa fa-credit-card"> Submit Payment</i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
