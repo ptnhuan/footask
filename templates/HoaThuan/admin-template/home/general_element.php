ff6b504bfb6abfd9945d66b78b3368d9424c0a07
ThuanNguyen1996 <nguyenthuan9613@gmail.com>
2017-04-26 19:58:27 +0700

    commit admin template

--- templates/HoaThuan/admin-template/home/general_element.php
+++ templates/HoaThuan/admin-template/home/general_element.php
@@ -183,8 +183,102 @@
                                 <div class="nav toggle">
                                     <a id="btn_menu"><i class="fa fa-bars"></i></a>
                                 </div>
-                                <ul class="nav navbar-nav navbar-right" style="height: 50px;">
-                                    <!--...........................................-->
+                                <ul class="nav navbar-nav navbar-right">
+                                    <div class="drop">
+                                        <div type="button" class="bt" data-toggle="collapse" data-target="#john">
+                                            <a>
+                                                <img src="images/img.jpg" alt="">John Doe
+                                                <span class="fa fa-chevron-down"></span>
+                                            </a>
+                                            <h5>&nbsp;</h5>
+                                        </div>
+                                        <div id="john" class="collapse">
+                                            <div class="c_john">
+                                                <ul>
+                                                    <li><a> Profile</a></li>
+                                                    <li>
+                                                        <a>
+                                                            <span class="badge bg-red ">50%</span>
+                                                            <span>Settings</span>
+                                                        </a>
+                                                    </li>
+                                                    <li><a>Help</a></li>
+                                                    <li><a><i class="fa fa-sign-out "></i> Log Out</a></li>
+                                                </ul>                
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="drop">
+                                        <div type="button" class="bt" data-toggle="collapse" data-target="#mail">
+                                            <a>
+                                                <i class="fa fa-envelope-o"></i>
+                                                <span class="badge bg-green">6</span>
+                                            </a>
+                                            <h5>&nbsp;</h5>
+                                        </div>
+                                        <div id="mail" class="collapse">
+                                            <div class="c_mail">
+                                                <ul>
+                                                    <li class="ago">
+                                                        <a>
+                                                            <span class="image"><img src="images/img.jpg" alt="Profile Image"></span>
+                                                            <span>
+                                                                <span>John Smith</span>
+                                                                <span class="time">3 mins ago</span>
+                                                            </span>
+                                                            <span class="message">
+                                                                Film festivals used to be do-or-die moments for movie makers. They were where...
+                                                            </span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="ago">
+                                                        <a>
+                                                            <span class="image"><img src="images/img.jpg" alt="Profile Image"></span>
+                                                            <span>
+                                                                <span>John Smith</span>
+                                                                <span class="time">3 mins ago</span>
+                                                            </span>
+                                                            <span class="message">
+                                                                Film festivals used to be do-or-die moments for movie makers. They were where...
+                                                            </span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="ago">
+                                                        <a>
+                                                            <span class="image"><img src="images/img.jpg" alt="Profile Image"></span>
+                                                            <span>
+                                                                <span>John Smith</span>
+                                                                <span class="time">3 mins ago</span>
+                                                            </span>
+                                                            <span class="message">
+                                                                Film festivals used to be do-or-die moments for movie makers. They were where...
+                                                            </span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="ago">
+                                                        <a>
+                                                            <span class="image"><img src="images/img.jpg" alt="Profile Image"></span>
+                                                            <span>
+                                                                <span>John Smith</span>
+                                                                <span class="time">3 mins ago</span>
+                                                            </span>
+                                                            <span class="message">
+                                                                Film festivals used to be do-or-die moments for movie makers. They were where...
+                                                            </span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="ago">
+                                                        <div class="text-center">
+                                                            <a>
+                                                                <strong>See All Alerts</strong>
+                                                                <i class="fa fa-angle-right"></i>
+                                                            </a>
+                                                        </div>
+                                                    </li>
+                                                </ul>                
+                                            </div>
+                                        </div>
+                                    </div>
                                 </ul>
                             </nav>
                         </div>
